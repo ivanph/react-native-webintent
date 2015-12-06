9cf63bd8168d5135d781f0bb0c012a0431a58b3e
Ivan Pintado <ivan.pintadoh@gmail.com>
2015-12-05 21:43:26 -0800

    Upgrade example app to react-native v0.16

--- WebIntentExample/android/app/src/main/java/com/webintentexample/MainActivity.java
+++ WebIntentExample/android/app/src/main/java/com/webintentexample/MainActivity.java
@@ -74,7 +74,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
         super.onResume();
 
         if (mReactInstanceManager != null) {
-            mReactInstanceManager.onResume(this);
+            mReactInstanceManager.onResume(this, this);
         }
     }
 }
